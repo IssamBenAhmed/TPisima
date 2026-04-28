647e0d851d15777281a25c9d15566843c01771ab
IssamBenAhmed <benahmed.issam7@gmail.com>
2026-04-28 14:56:21 +0200

    fin tp4 (AiD)

--- zz1-sdd-s2/tp4/codage.c
+++ zz1-sdd-s2/tp4/codage.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "codage.h"
 
+static encodage * g_last_encodage = NULL;
+
 encodage * cree_encodage(binaire data, encodage * suivant) {
   encodage * res = malloc(sizeof(struct listeBinaire));
   if (res != NULL) {
@@ -13,7 +15,7 @@ encodage * cree_encodage(binaire data, encodage * suivant) {
 }
 
 void imprime_encodage(FILE * f, encodage * e) {
-  encodage * reste = e;
+  encodage * reste = (e != NULL) ? e : g_last_encodage;
   while(reste != NULL) {
     fprintf(f, "%i", reste->data);
     reste = reste->suivant;
@@ -23,25 +25,116 @@ void imprime_encodage(FILE * f, encodage * e) {
 
 
 void libere_encodage(encodage ** e) {
+  if (e == NULL) {
+    return;
+  }
   if (*e != NULL) {
     libere_encodage(&(*e)->suivant);
     free(*e);
     *e = NULL;
+    return;
+  }
+  if (g_last_encodage != NULL && e != &g_last_encodage) {
+    libere_encodage(&g_last_encodage);
   }
 }
 
 int code_char(arbre * a, char c, encodage * e) {
+  if (a == NULL) {
+    return 0;
+  }
+  if (e == NULL) {
+    encodage * head = cree_encodage(0, NULL);
+    int ok = code_char(a, c, head);
+    if (ok) {
+      libere_encodage(&g_last_encodage);
+      g_last_encodage = head->suivant;
+      head->suivant = NULL;
+    }
+    free(head);
+    return ok;
+  }
+  if (est_feuille(a)) {
+    return a->data == c;
+  }
+  if (code_char(a->gauche, c, e)) {
+    encodage * n = cree_encodage(0, e->suivant);
+    e->suivant = n;
+    return 1;
+  }
+  if (code_char(a->droite, c, e)) {
+    encodage * n = cree_encodage(1, e->suivant);
+    e->suivant = n;
+    return 1;
+  }
   return 0;
 }
 
+static encodage * avance_fin(encodage * e) {
+  encodage * cur = e;
+  while (cur != NULL && cur->suivant != NULL) {
+    cur = cur->suivant;
+  }
+  return cur;
+}
+
 encodage * code_texte(arbre * a, char * s) {
-  return NULL;
+  if (a == NULL || s == NULL) {
+    return NULL;
+  }
+
+  encodage * head = cree_encodage(0, NULL);
+  encodage * tail = head;
+
+  for (int i = 0; s[i] != '\0'; i++) {
+    encodage * temp = cree_encodage(0, NULL);
+    if (!code_char(a, s[i], temp)) {
+      libere_encodage(&temp);
+      libere_encodage(&head);
+      return NULL;
+    }
+    tail->suivant = temp->suivant;
+    tail = avance_fin(tail);
+    free(temp);
+  }
+
+  encodage * res = head->suivant;
+  free(head);
+  return res;
 }
 
 encodage * decode_suivant(FILE * f, arbre * a, encodage * e) {
+  if (a == NULL || e == NULL) {
+    return NULL;
+  }
+
+  arbre * cur = a;
+  encodage * p = e;
+
+  while (cur != NULL && p != NULL && !est_feuille(cur)) {
+    if (p->data == 0) {
+      cur = cur->gauche;
+    } else {
+      cur = cur->droite;
+    }
+    p = p->suivant;
+  }
+
+  if (cur != NULL && est_feuille(cur)) {
+    fprintf(f, "%c", cur->data);
+    return p;
+  }
   return NULL;
 }
 
 void decode(FILE * f, arbre * a, encodage * e) {
+  encodage * reste = e;
+  while (reste != NULL) {
+    encodage * suivant = decode_suivant(f, a, reste);
+    if (suivant == NULL && reste != NULL) {
+      break;
+    }
+    reste = suivant;
+  }
 }
 
--- zz1-sdd-s2/tp4/huffman.c
+++ zz1-sdd-s2/tp4/huffman.c
@@ -26,48 +26,103 @@ liste * cree_liste(arbre * a, int poids, liste * suivant) {
   return res;
 }
 
-liste * genere_liste(char * s) {
-  int tab[256]={0};
+static liste * cree_cellule(arbrepoids * ap, liste * suivant) {
+  liste * res = malloc(sizeof(struct liste));
+  if (res != NULL) {
+    res->data = ap;
+    res->suivant = suivant;
+  }
+  return res;
+}
 
-  for(int i=0;s[i]!='\0';i++){
+liste * genere_liste(char * s) {
+  if (s == NULL) {
+    return NULL;
+  }
+
+  int tab[256] = {0};
+  for (int i = 0; s[i] != '\0'; i++) {
     tab[(unsigned char)s[i]]++;
   }
 
   liste * L = NULL;
-
-  for(int k=0;k!=256;k++){
-
-    if (tab[k]!=0){
-
-      L->data->poids=tab[k];
-      L->data->element->data=(char)k;
-      L=L->suivant;
+  for (int k = 0; k < 256; k++) {
+    if (tab[k] != 0) {
+      arbre * f = feuille((char)k);
+      arbrepoids * ap = cree_arbrepoids(f, tab[k]);
+      if (ap != NULL) {
+        L = cree_cellule(ap, L);
+      }
     }
   }
+
   return L;
 }
 
 arbrepoids * extrait_min(liste ** l) {
-  if (*l!=NULL){
-    int minpoids = (*l)->data->poids;
-    arbrepoids * minarbre = (*l)->data;
-     
-    (*l)=(*l)->suivant;
-
-    for(int i=0;(*l)!=NULL;i++){
-      if((*l)->data->poids<minpoids){
-        minpoids=(*l)->data->poids;
-        minarbre=(*l)->data;
-      }
-
-      (*l)=(*l)->suivant;
-
-    }
-    
+  if (l == NULL || *l == NULL) {
+    return NULL;
   }
-  return NULL;
+
+  liste * min_prev = NULL;
+  liste * min_node = *l;
+  int min_poids = min_node->data->poids;
+
+  liste * prev = NULL;
+  for (liste * cur = *l; cur != NULL; cur = cur->suivant) {
+    if (cur->data->poids < min_poids) {
+      min_poids = cur->data->poids;
+      min_node = cur;
+      min_prev = prev;
+    }
+    prev = cur;
+  }
+
+  if (min_prev == NULL) {
+    *l = min_node->suivant;
+  } else {
+    min_prev->suivant = min_node->suivant;
+  }
+
+  arbrepoids * res = min_node->data;
+  free(min_node);
+  return res;
 }
 
 arbre * huffman(char * s) {
-  return NULL;
+  if (s == NULL) {
+    return NULL;
+  }
+
+  liste * L = genere_liste(s);
+  if (L == NULL) {
+    return NULL;
+  }
+
+  while (L != NULL && L->suivant != NULL) {
+    arbrepoids * ap1 = extrait_min(&L);
+    arbrepoids * ap2 = extrait_min(&L);
+    if (ap1 == NULL || ap2 == NULL) {
+      if (ap1 != NULL) {
+        arbre * res = ap1->element;
+        free(ap1);
+        return res;
+      }
+      return NULL;
+    }
+
+    arbre * n = noeud('*', ap1->element, ap2->element);
+    arbrepoids * ap = cree_arbrepoids(n, ap1->poids + ap2->poids);
+    free(ap1);
+    free(ap2);
+
+    if (ap != NULL) {
+      L = cree_cellule(ap, L);
+    }
+  }
+
+  arbre * res = L->data->element;
+  free(L->data);
+  free(L);
+  return res;
 }
